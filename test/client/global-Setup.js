521137d2a3a46b34cd0f103fb1e8bac7b5202c31
Abou <nasmia.study@gmail.com>
2024-10-31 23:57:00 +0100

    Global authentication automation

---
+++ test/client/global-Setup.js
@@ -0,0 +1,20 @@
+
+import {  chromium, expect} from '@playwright/test';
+
+async function globalSetup(){
+const browser = await chromium.launch({headed:true});
+const context = await browser.newContext();
+const page= await context.newPage();
+await page.goto('http://localhost:5173/auth/login');
+await page.getByPlaceholder("Enter your email").fill("anne.test@gmail.com");
+await page.getByPlaceholder("Enter your password").fill("@@anne@@");
+await page.getByRole('button',{name: 'Sign In'}).click();
+
+await expect(page).toHaveURL("http://localhost:5173/shop/home");
+
+await page.context().storageState({path:"./.auth/user.json"})
+
+await browser.close();
+}
+
+export default globalSetup;
